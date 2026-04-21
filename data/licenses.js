f3732237b4af464dd059aaa9516871aa44cf8684
unknown <arthurdanilotavares@hotmail.com>
2026-04-21 15:15:53 -0300

    CrocoN

--- data/licenses.js
+++ data/licenses.js
@@ -5,16 +5,24 @@ const tribeLicenses = [
     tribeTag: "Asd",
     tribeName: "King",
     active: true,
-    expiresAt: "2026-12-31T23:59:59.000Z"
+    expiresAt: "2026-12-31T23:59:59.000Z",
   },
-    {
+  {
     world: "br138",
     tribeId: "710",
     tribeTag: "CrocoS",
     tribeName: "CROCODILO SUL",
     active: true,
-    expiresAt: "2026-12-31T23:59:59.000Z"
-  }
+    expiresAt: "2026-12-31T23:59:59.000Z",
+  },
+  {
+    world: "br138",
+    tribeId: "43",
+    tribeTag: "CrocoN",
+    tribeName: "CROCODILO NORTE",
+    active: true,
+    expiresAt: "2026-12-31T23:59:59.000Z",
+  },
 ];
 
 module.exports = tribeLicenses;
